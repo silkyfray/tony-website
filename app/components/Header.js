42e05eb3af15ec2bf761d4f7df1b26a12bdbca7f
Antonio Nikolov <anikolovn@gmail.com>
2017-03-08 23:47:31 +0000

    made a start on the css for the navigation bar

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,8 +1,16 @@
 import React from "react"
-import {styles} from "../styles/index"
+import { styles } from "../styles/index"
 
 export default function Header(props) {
     return (
-        <h1 style={styles.bigGlow}>{props.title}</h1>
+        <div>
+            <h1 style={styles.bigGlow}>{props.title}</h1>
+            <ul className="navigation-bar">
+                <li>Projects</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+
     )
 }
