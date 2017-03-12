aa895685ad369cd4c8aaf41695ffbb19752dee5a
Antonio Nikolov <anikolovn@gmail.com>
2017-03-12 23:27:19 +0000

    Added real projects to replace the dummy dummy dummy.

--- app/components/ProjectShort.jsx
+++ app/components/ProjectShort.jsx
@@ -1,22 +1,28 @@
 import React, { PropTypes } from "react"
 import styles from '../styles/ProjectShort.css';
 
+function ShareButton(props) {
+    return (
+        <a href={props.destinationUrl}>
+            <img src={props.buttonUrl} className={styles.shareButton} />
+        </a>
+    )
+}
+
 export default function ProjectShort(props) {
-    var picture;
-    if (props.spec.picture) {
-        picture = <img src={props.spec.picture}
-            className={styles.picture} />
-    }
-    else {
-        picture = <div className={styles.picture} />
-    }
+    var picture = props.spec.pictureUrl ? <img src={props.spec.pictureUrl} className={styles.picture} />
+        : picture = <div className={styles.picture} />;
+
     return (
         <div className={styles.outerBox}>
             <div className={styles.innerBox}>
                 <div className={styles.topInfo}>
                     <h3 className={styles.header}>{props.spec.header}</h3>
                     {picture}
-                    <div className={styles.subheader}>{props.spec.subheader}</div>
+                    <div className={styles.subheader}>{props.spec.year}|{props.spec.category}</div>
+                    {props.spec.githubUrl && <ShareButton destinationUrl={props.spec.githubUrl} buttonUrl="/data/img/githubIcon.svg" />}
+                    {props.spec.youtubeUrl && <ShareButton destinationUrl={props.spec.youtubeUrl} buttonUrl="/data/img/youtubeIcon.svg" />}
+                    {props.spec.documentUrl && <ShareButton destinationUrl={props.spec.documentUrl} buttonUrl="/data/img/documentIcon.svg" />}
                 </div>
                 <p>{props.spec.summary}</p>
             </div>
@@ -28,8 +34,12 @@ export default function ProjectShort(props) {
 ProjectShort.propTypes = {
     spec: PropTypes.shape({
         header: PropTypes.string.isRequired,
-        picture: PropTypes.string,
-        subheader: PropTypes.string.isRequired,
+        pictureUrl: PropTypes.string,
+        year: PropTypes.number.isRequired,
+        category: PropTypes.string.isRequired,
         summary: PropTypes.string.isRequired,
+        githubUrl: PropTypes.string,
+        youtubeUrl: PropTypes.string,
+        documentUrl: PropTypes.string
     })
 }
