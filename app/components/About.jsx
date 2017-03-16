8abfbff198d492385c33f7d573bcd9b483e7bdfe
Antonio Nikolov <anikolovn@gmail.com>
2017-03-16 22:59:05 +0000

    Made a start on the snippets functionality.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,22 +1,57 @@
 import React from "react"
 import styles from '../styles/About.css';
+import Typist from 'react-typist'
+import 'react-typist/dist/Typist.css'
 
-export default function About(props) {
+import AboutData from '../data/aboutSnippets.json'
+
+function InputButton(props) {
     return (
-        <div className={styles.contentArea}>
-            <div className={styles.chatBoxContainer}>
-                <div className={styles.chatBoxMessages}>
+        <span className={styles.chatBoxInputButton}
+            onClick={props.onClickHandler}>
+            <img src={props.imageUrl} />
+            {props.children}
+        </span>
+    )
+}
 
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id varius leo, id facilisis massa. Praesent suscipit molestie tortor quis vulputate. Nullam maximus ligula lacus, non volutpat urna viverra quis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi nunc enim, consequat sed sodales vitae, dictum sit amet metus. Vivamus nec ipsum in lectus accumsan feugiat. Donec vehicula vulputate urna quis aliquam. Vivamus dapibus, felis pharetra lobortis gravida, diam nibh facilisis lectus, eu auctor tortor urna quis elit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus ut sapien in diam faucibus hendrerit non nec dolor.
-
-Nam sit amet velit vitae urna vestibulum viverra et id ex. Morbi cursus erat sit amet augue congue vestibulum. Maecenas gravida gravida lorem, quis porta justo pretium vitae. Morbi consectetur orci vitae turpis venenatis placerat. Integer sit amet neque sed risus suscipit malesuada eget nec nisi. Nam ornare id massa eget ornare. Proin vitae cursus libero, ut ultrices metus.
-
-Sed ac pellentesque dolor, a posuere leo. Donec porttitor luctus molestie. Morbi elementum posuere enim a maximus. Curabitur lacus dolor, tincidunt vel rhoncus commodo, dictum placerat mi. Donec egestas orci id magna faucibus sollicitudin. Etiam condimentum rhoncus justo, et ornare nisi maximus a. In ac porttitor arcu. Sed feugiat accumsan felis sit amet dapibus. Maecenas vel lorem id velit pellentesque vestibulum. Sed pretium nisi at sem consectetur suscipit. Etiam pulvinar pellentesque purus, at porta leo dignissim sed. Fusce nulla sem, congue volutpat urna scelerisque, auctor cursus ipsum. Nam pulvinar diam nibh, id iaculis sapien pretium ut. Ut sed varius leo. Fusce eget congue quam, eget congue nulla. Nulla facilisi.
-                </div>
-                <div className={styles.chatBoxInput}>
-
+export default class About extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            snippets: []
+        }
+    }
+    componentDidMount() {
+        this.setState({ snippets: AboutData })
+    }
+    // Input handlers
+    onNextHandler() {
+        console.log("next button pressed");
+    }
+    onSkipHandler() {
+        console.log("skip button pressed");
+    }
+    render() {
+        return (
+            <div className={styles.contentArea}>
+                <div className={styles.chatBoxContainer}>
+                    <div className={styles.chatBoxMessages}>
+                        {/*Place snippets here*/}
+                    </div>
+                    <div className={styles.chatBoxInput}>
+                        <InputButton imageUrl="/data/img/ic_play_arrow_white_36px.svg"
+                            onClickHandler={this.onNextHandler}>
+                            Next
+                </InputButton>
+                        <InputButton imageUrl="/data/img/ic_fast_forward_white_36px.svg"
+                            onClickHandler={this.onSkipHandler}>
+                            Skip to the end
+                </InputButton>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
+
 }
