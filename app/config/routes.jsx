9b67d0b014bf21f4ac8251abea8e8921bf56efb7
Antonio Nikolov <anikolovn@gmail.com>
2017-03-18 20:34:17 +0000

    added google analytics to router.

--- app/config/routes.jsx
+++ app/config/routes.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 // import ReactRouter from "react-router"
-import {ReactRouter, Router, Route, IndexRoute, hashHistory} from 'react-router';
+import { ReactRouter, Router, Route, IndexRoute, hashHistory } from 'react-router';
 import Main from "../components/Main"
 import About from "../components/About"
 import ProjectsContainer from "../components/ProjectsContainer"
 import Contact from "../components/Contact"
 
+var ReactGA = require('react-ga');
+ReactGA.initialize('UA-38975658-2', {
+    // debug: true
+});
+
+function logPageView() {
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+}
+
 var routes = (
-    <Router history= {hashHistory}>
+    <Router history={hashHistory} onUpdate={logPageView}>
         <Route path="/" component={Main}>
             <IndexRoute component={ProjectsContainer} />
-            <Route path="/projects" component={ProjectsContainer}/>
-            <Route path="/about" component={About}/>
+            <Route path="/projects" component={ProjectsContainer} />
+            <Route path="/about" component={About} />
             {/*<Route path="/contact" component={Contact}/>*/}
         </Route>
     </Router>
