538fe6a4f5e7cba3269c8f2a42f9d0223502de10
Antonio Nikolov <anikolovn@gmail.com>
2017-03-19 18:35:11 +0000

    Added linked in link

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -16,7 +16,8 @@ function NavLink(props) {
 export default function Header(props) {
     return (
         <div style={{position: "relative"}}>
-            <h1>{props.title}</h1>
+            <h1 style={{display: "inline"}}>{props.title}</h1>
+            <a href="https://www.linkedin.com/in/antonionikolov"><img src='/data/img/Linkedin.png' width="32px" height="32px" style={{ marginLeft: "10px"}}/></a>
             <div style={{ fontSize: "10px", position: "absolute", right:"15px", top:"15px" }}>
                 App created with <a href="https://facebook.github.io/react/"><i>React.js</i></a>
             </div>
